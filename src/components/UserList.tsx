3537d4bbb2ecd4e039668bfb4d64abbbe25473ee
PezoDen <pezo1981@gmail.com>
2021-04-26 20:47:55 +0300

    userList

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTypedSelector} from "../store/hook/useTypedSelector";
+import {useDispatch} from "react-redux";
+import {fetchUsers} from "../store/action-creator/user";
 
-const UserList:React.FC = () => {
-    const state = useTypedSelector(state => state)
-    console.log(state)
+const UserList: React.FC = () => {
+    const {users, error, loading} = useTypedSelector(state => state.user)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if (loading) {
+        return <h1>...Загрузка...</h1>
+    }
+    if (error) {
+        return <h1>{error}</h1>
+    }
     return (
         <div>
-            
+            {users.map(u =>
+                <div>{u.name}</div>)}
         </div>
     );
 };
